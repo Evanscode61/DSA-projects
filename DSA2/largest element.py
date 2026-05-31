7f094f30784b0826ba2095a3dca5478b38dd4767
Evanscode61 <evancekibe53@gmail.com>
2026-05-31 23:24:57 +0300

    finding largest element in an array

---
+++ DSA2/largest element.py
@@ -0,0 +1,9 @@
+class Solution:
+    def largestNumber(self, arr):
+        return max(arr)
+    # time complexity o(n) and space complexity of o(1)
+sol = Solution()
+print(sol.largestNumber([34,56,76,34,89]))
+
+
+
